2b70d38e55cfcf810ea176acc04bf73a10540b5f
daniel.i.scarr <daniel.i.scarr@gmail.com>
2018-08-19 15:42:55 -0500

    Add project files.

---
+++ CountAPI/Controllers/CountingController.cs
@@ -0,0 +1,35 @@
+﻿using CountAPI.Modles;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace CountAPI.Controllers
+{
+    [Route("api/Count")]
+    [ApiController]
+    public class CountController : ControllerBase
+    {
+
+
+        [HttpPost]
+        public JObject Count(CountedText item)
+        {
+            string[] jsonResults = new string[2];
+
+            //original text that was GOT
+            jsonResults[0] = item.Text;
+
+            //Count the characters
+            jsonResults[1] = item.Text.Length.ToString();
+
+            //Split the text into parse-able bits, original text is first, calculated count of text is second
+            string jsonString = "{" +
+                "OriginalText: '" + jsonResults[0] + "'," +
+                "Count: '" + jsonResults[1] + "'" +
+                "}";
+
+            JObject j = JObject.Parse(jsonString);
+            return j;
+        }
+
+    }
+}
---
+++ CountAPI/Models/CountedText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountAPI.Modles
+{
+	public class CountedText
+	{
+		public string Name { get; set; }
+		public string Text { get; set; }
+	}
+}
